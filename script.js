6afa0ccc3587bf283e11abedff050c99a068d97d
isac <210512248+shfuitck414@users.noreply.github.com>
2026-05-03 03:35:36 +0800

    Make functioning buttons and screen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function add(a, b) {
     return a + b
 }
 
-function substract(a, b) {
+function subtract(a, b) {
     return a - b
 }
 
@@ -15,12 +15,18 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+    console.log(`a = ${a}`)
+    console.log(`b = ${b}`)
+    console.log(`operator = ${operator}`)
+    console.log(operator(+a, +b))
     return operator(+a, +b)
 }
 
-let a = 0
+let a = null
 let b = null
 let operator = null
+
+screen = document.querySelector(".screen")
 
 button = document.querySelector(".container")
 
@@ -30,21 +36,86 @@ button.addEventListener("click", e => {
             console.log(e.target.textContent)
             if (b == null && operator != null) {
                 b = e.target.textContent
+                screen.textContent = b
             } else if(b != null && operator != null) {
-                a = operate(a, b, operator)
-                b = null
-                operator = null
+                if (b == 0 || b == '') {
+                    b = e.target.textContent
+                }else {
+                    b += e.target.textContent
+                }
+                screen.textContent = b
             } else if(operator == null) {
-                a += e.target.textContent
+                if (a == null || a == "0") {
+                    a = e.target.textContent
+                } else if (a != null) {
+                    a += e.target.textContent
+                }
+                screen.textContent = a
             }
         } else if(e.target.classList.contains('operator')) {
+            if(operator == null) {
+                if(e.target.textContent == "+") {
+                    operator = add
+                } else if(e.target.textContent == "-") {
+                    operator = subtract
+                } else if(e.target.textContent == "x") {
+                    operator = multiply
+                } else if(e.target.textContent == "÷") {
+                    operator = divide
+                }
+            } else if(operator != null) {
+                if (b == null) {
+                    if(e.target.textContent == "+") {
+                        operator = add
+                    } else if(e.target.textContent == "-") {
+                        operator = subtract
+                    } else if(e.target.textContent == "x") {
+                        operator = multiply
+                    } else if(e.target.textContent == "÷") {
+                        operator = divide
+                    }
+                } else if (b != null) {
+                    a = operate(a, b, operator)
+                    b = null
+                }
+            }
             
         } else if(e.target.classList.contains('equal')) {
-            console.log(3)
+            if (b != null && operator != null){
+                screen.textContent = operate(a, b, operator)
+                a = null
+                b = null
+                operator = null
+            }
         } else if(e.target.classList.contains('delete')) {
-            console.log(4)
+            if (b != null) {
+                if (b == null || b.length == 1) {
+                    b = 0
+                    console.log(b)
+                    screen.textContent = ''
+                } else if (b.length > 1) {
+                    b = b.substring(0, b.length - 1)
+                    console.log(b)
+                    screen.textContent = b
+                } 
+            } else if (b == null && operator == null) {
+
+                if (a == null ||a.length == 1) {
+                    a = 0
+                    console.log(a)
+                    screen.textContent = ''
+                } else if (a.length > 1) {
+                    a = a.substring(0, a.length - 1)
+                    console.log(a)
+                    screen.textContent = a
+                }
+            }
+
         } else if(e.target.classList.contains('clear')) {
-            console.log(5)
+            a = null
+            b = null
+            operator = null
+            screen.textContent = 0
         }
     }
     })
